0ae4dc268cf330ad90e45aa3b0069e3d4f5dc7b9
nawaphon60 <nawaphonwichaidit@users.noreply.github.com>
2020-11-12 17:03:17 +0700

    17.03

--- src/app/components/add-edit-delivery/add-edit-delivery.module.ts
+++ src/app/components/add-edit-delivery/add-edit-delivery.module.ts
@@ -5,7 +5,11 @@ import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzListModule } from 'ng-zorro-antd/list';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzSwitchModule } from 'ng-zorro-antd/switch'
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 
 
@@ -17,7 +21,11 @@ import { NzSwitchModule } from 'ng-zorro-antd/switch';
     NzListModule,
     NzSelectModule,
     NzIconModule,
-    NzSwitchModule
+    NzSwitchModule,
+    NzInputModule,
+    NzTagModule,
+    NzButtonModule,
+    NzGridModule
   ],
   exports: [
     AddEditDeliveryComponent
